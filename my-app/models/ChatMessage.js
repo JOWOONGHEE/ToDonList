3ad84406eabcba9d35d2bd3072d974502925d134
WoongHee Jo <xcv415@naver.com>
2024-05-17 14:15:06 +0900

    merge

--- my-app/models/ChatMessage.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ChatMessageSchema = new mongoose.Schema({
-  role: String,
-  content: String,
-  timestamp: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('ChatMessage', ChatMessageSchema);
